b2be0c0be348d4178c1d8b0639b66bd107e6c9cb
stoutimon <151385386+stoutimon@users.noreply.github.com>
2026-05-21 01:32:12 +0200

    Update start.js

--- start.js
+++ start.js
@@ -5,15 +5,15 @@ module.exports = async (kernel) => {
       "method": "shell.run",
       "params": {
         "path": ".",
-        "message": "openclaw start",
+        "message": "openclaw gateway start",
         "on": [
-          { "event": "/Gateway running|server started/i", "done": true }
+          { "event": "/Gateway online|gateway started|running/i", "done": true }
         ]
       }
     }, {
       "method": "local.set",
       "params": {
-        "url": "http://127.0.0.1:3000"   // Change if OpenClaw uses a different default port
+        "url": "http://127.0.0.1:18789"
       }
     }]
   }
