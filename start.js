29968b73571e2f5ccfab8ab6513a005b6f396b5e
stoutimon <151385386+stoutimon@users.noreply.github.com>
2026-05-20 18:30:04 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,21 +1,33 @@
-module.exports = async (kernel) => {
-  const PORT = await kernel.port()
-  return {
-    "daemon": true,
-    "run": [{
+module.exports = {
+    daemon: true,
+    run: [{
       "method": "shell.run",
       "params": {
-        "path": "app",
-        "venv": "env",
-        "message": `open-webui serve --port 42004 --host 127.0.0.1`,
-        //"on": [{ "event": "/http://[0-9.:]+/", "done": true }]
-        "on": [{ "event": "/Started server process/i", "done": true }]
+        "message": [
+          "openclaw gateway run"
+        ],
+        "on": [{
+          "event": "/listening on.*ws:\\/\\/([0-9.:]+)/",
+          "done": true
+        }]
       }
-    }, {
+    },
+    {
+      "method": "shell.run",
+      "params": {
+        "message": [
+          "openclaw dashboard"
+        ],
+        "on": [{
+          "event": "/(http:\\/\\/\\S+)/",
+          "done": true
+        }]
+      }
+    },
+    {
       "method": "local.set",
       "params": {
-        "url": `http://127.0.0.1:42004`
+        "url": "{{input.event[1]}}"
       }
     }]
-  }
 }
